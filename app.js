a08b9c717b65e867b1863b96d26b59c456976829
Erin Ranta <erinran99@gmail.com>
2022-05-30 18:02:56 -0400

    wrapped in functions

--- app.js
+++ app.js
@@ -8,6 +8,43 @@
 
 
 
+function askFood() {
+favoriteFood = prompt("What is your favorite food?")
+console.log(favoriteFood);
+document.write(favoriteFood + " is " + " yummy " + " but " + " nachos " + " are " + " better! ");
+
+}
+
+
+
+
+
+function customMessage(){
+    let message = "";
+    if (favoriteFood == "nachos"){
+        message = "YUM!"
+        document.write(message);
+     } else if (favoriteFood == "tacos") {
+            message = "ooooooo!";
+            document.write(message);
+     } else {
+         message = "best!";
+         document.write(message);
+     }
+
+        }
+
+        
+    
+customMessage();
+askFood();
+    
+    
+        
+    
+
+
+
 
 
 
